972a3d0fb65aa6f2a247fc1255c212db2c5b2629
sefa65 <sefa65130@gmail.com>
2016-07-23 05:42:28 +0000

    rework general structure for more readability

--- dist.js
+++ dist.js
@@ -1,509 +1,50 @@
+var towns = [
+        [0,16,16,8,27,19,27,39,43,22,42,39,54,60,52,74],
+        [16,0,18,12,20,28,28,43,50,11,27,23,39,44,37,61],
+        [16,18,0,24,23,12,20,31,35,18,42,40,53,59,53,79],
+        [8,12,24,0,9,29,22,38,46,40,19,19,30,36,30,63],
+        [27,20,23,9,0,25,14,36,38,36,36,27,33,40,36,71],
+        [19,28,12,29,25,0,19,19,23,12,48,47,58,66,59,89],
+        [27,28,20,22,14,19,0,16,24,26,40,40,48,54,49,85],
+        [39,43,31,38,36,19,16,0,4,27,54,56,61,68,64,100],
+        [43,50,35,46,38,23,24,4,0,26,62,64,69,77,74,108],
+        [22,11,18,40,36,12,26,27,26,0,59,59,69,75,70,96],
+        [42,27,42,19,36,48,40,54,62,59,0,8,12,17,11,50],
+        [39,23,40,19,27,47,40,56,64,59,8,0,16,22,14,45],
+        [54,39,53,30,33,58,48,61,69,69,12,19,0,7,10,52],
+        [60,44,59,36,40,65,54,68,77,75,17,22,7,0,9,46],
+        [52,37,53,30,36,59,49,64,74,70,11,14,10,9,0,41],
+        [74,61,79,63,71,89,85,100,108,96,50,45,52,46,41,0]
+    ];
+
 $(document).ready(function(){
+    $("form").submit(function(){return false;});
     $("select").change(function(){
-        setTime($('#start').val(),$("#stop").val(),$("#trans").val());
-    })
-})
+        setTime($('#start').val(),$("#stop").val(),$("#trans").val(),parseInt($("#hours").val()));
+    });
+    $("#hours").change(function(){
+        var hours = parseInt($("#hours").val());
+        if ( !isNaN(hours) && 0 < hours && hours <= 24 ){
+            setTime($('#start').val(),$("#stop").val(),$("#trans").val(),Math.floor(hours));
+        }
+        else{
+            setTime($('#start').val(),$("#stop").val(),$("#trans").val(),9);
+            $("#hours").val(9);
+            $("#hours").html(9);
+            $("#hours").text(9);
+        }
+    });
+});
 
 
-function setTime(a,b,c){
+function setTime(a,b,c,d){
     if (a==b){
         $('#time').html('0 heures');
         return;
     }
-    var tmp;
-    if (parseInt(b)<parseInt(a)){
-        tmp = b;
-        b = a;
-        a= tmp;
-    }
-    switch(a){
-        case "0":
-            {
-                switch(b){
-                    case "1":
-                    case "2":
-                        tmp = 16;
-                        break;
-                    case "3":
-                        tmp = 8;
-                        break;
-                    case "4":
-                    case "6":
-                        tmp = 27;
-                        break;
-                    case "5":
-                        tmp = 19;
-                        break;
-                    case "7":
-                        tmp = 39;
-                        break;
-                    case "8":
-                        tmp = 43;
-                        break;
-                    case "9":
-                        tmp = 22;
-                        break;
-                    case "10":
-                        tmp = 42;
-                        break;
-                    case "11":
-                        tmp = 39;
-                        break;
-                    case "12":
-                        tmp = 54;
-                        break;
-                    case "13":
-                        tmp = 60;
-                        break;
-                    case "14":
-                        tmp = 52;
-                        break;
-                    case "15":
-                        tmp = 74;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "1":
-            {
-                switch(b){
-                    case "2":
-                        tmp = 18;
-                        break;
-                    case "3":
-                        tmp = 12;
-                        break;
-                    case "4":
-                        tmp = 20;
-                        break;
-                    case "5":
-                    case "6":
-                        tmp = 28;
-                        break;
-                    case "7":
-                        tmp = 43;
-                        break;
-                    case "8":
-                        tmp = 50;
-                        break;
-                    case "9":
-                        tmp = 11;
-                        break;
-                    case "10":
-                        tmp = 27;
-                        break;
-                    case "11":
-                        tmp = 23;
-                        break;
-                    case "12":
-                        tmp = 39;
-                        break;
-                    case "13":
-                        tmp = 44;
-                        break;
-                    case "14":
-                        tmp = 37;
-                        break;
-                    case "15":
-                        tmp = 61;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "2":
-            {
-                switch(b){
-                    case "3":
-                        tmp = 24;
-                        break;
-                    case "4":
-                        tmp = 23
-                        break;
-                    case "5":
-                        tmp = 12;
-                        break;
-                    case "6":
-                        tmp = 20;
-                        break;
-                    case "7":
-                        tmp = 31;
-                        break;
-                    case "8":
-                        tmp = 35;
-                        break;
-                    case "9":
-                        tmp = 18;
-                        break;
-                    case "10":
-                        tmp = 42;
-                        break;
-                    case "11":
-                        tmp = 40;
-                        break;
-                    case "12":
-                        tmp = 53;
-                        break;
-                    case "13":
-                        tmp = 59;
-                        break;
-                    case "14":
-                        tmp = 53;
-                        break;
-                    case "15":
-                        tmp = 79;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "3":
-            {
-                switch(b){
-                    case "4":
-                        tmp = 9;
-                        break;
-                    case "5":
-                        tmp = 29;
-                        break;
-                    case "6":
-                        tmp = 22;
-                        break;
-                    case "7":
-                        tmp = 38;
-                        break;
-                    case "8":
-                        tmp = 46;
-                        break;
-                    case "9":
-                        tmp = 40;
-                        break;
-                    case "10":
-                    case "11":
-                        tmp = 19;
-                        break;
-                    case "12":
-                    case "14":
-                        tmp = 30;
-                        break;
-                    case "13":
-                        tmp = 36;
-                        break;
-                    case "15":
-                        tmp = 63;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "4":
-            {
-                switch(b){
-                    case "5":
-                        tmp = 25;
-                        break;
-                    case "6":
-                        tmp = 14;
-                        break;
-                    case "7":
-                    case "9":
-                    case "10":
-                    case "14":
-                        tmp = 36;
-                        break;
-                    case "8":
-                        tmp = 48;
-                        break;
-                    case "11":
-                        tmp = 27;
-                        break;
-                    case "12":
-                        tmp = 33;
-                        break;
-                    case "13":
-                        tmp = 40;
-                        break;
-                    case "15":
-                        tmp = 71;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-            case "5":
-            {
-                switch(b){
-                    case "6":
-                    case "7":
-                        tmp = 19;
-                        break;
-                    case "8":
-                        tmp = 23;
-                        break;
-                    case "9":
-                        tmp = 12;
-                        break;
-                    case "10":
-                        tmp = 48;
-                        break;
-                    case "11":
-                        tmp = 47;
-                        break;
-                    case "12":
-                        tmp = 58;
-                        break;
-                    case "13":
-                        tmp = 65;
-                        break;
-                    case "14":
-                        tmp = 59;
-                        break;
-                    case "15":
-                        tmp = 89;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "6":
-            {
-                switch(b){
-                    case "7":
-                        tmp = 16;
-                        break;
-                    case "8":
-                        tmp = 24;
-                        break;
-                    case "9":
-                        tmp = 26;
-                        break;
-                    case "10":
-                    case "11":
-                        tmp = 40;
-                        break;
-                    case "12":
-                        tmp = 48;
-                        break;
-                    case "13":
-                        tmp = 54;
-                        break;
-                    case "14":
-                        tmp = 49;
-                        break;
-                    case "15":
-                        tmp = 85;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "7":
-            {
-                switch(b){
-                    case "8":
-                        tmp = 4;
-                        break;
-                    case "9":
-                        tmp = 27;
-                        break;
-                    case "10":
-                        tmp = 54;
-                        break;
-                    case "11":
-                        tmp = 56;
-                        break;
-                    case "12":
-                        tmp = 61;
-                        break;
-                    case "13":
-                        tmp = 68;
-                        break;
-                    case "14":
-                        tmp = 64;
-                        break;
-                    case "15":
-                        tmp = 100;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "8":
-            {
-                switch(b){
-                    case "9":
-                        tmp = 26;
-                        break;
-                    case "10":
-                        tmp = 62;
-                        break;
-                    case "11":
-                        tmp = 64;
-                        break;
-                    case "12":
-                        tmp = 69;
-                        break;
-                    case "13":
-                        tmp = 77;
-                        break;
-                    case "14":
-                        tmp = 74;
-                        break;
-                    case "15":
-                        tmp = 108;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "9":
-            {
-                switch(b){
-                    case "10":
-                    case "11":
-                        tmp = 59;
-                        break;
-                    case "12":
-                        tmp = 69;
-                        break;
-                    case "13":
-                        tmp = 75;
-                        break;
-                    case "14":
-                        tmp = 70;
-                        break;
-                    case "15":
-                        tmp = 96;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "10":
-            {
-                switch(b){
-                    case "11":
-                        tmp = 8;
-                        break;
-                    case "12":
-                        tmp = 12;
-                        break;
-                    case "13":
-                        tmp = 17;
-                        break;
-                    case "14":
-                        tmp = 11;
-                        break;
-                    case "15":
-                        tmp = 50;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "11":
-            {
-                switch(b){
-                    case "12":
-                        tmp = 19;
-                        break;
-                    case "13":
-                        tmp = 22;
-                        break;
-                    case "14":
-                        tmp = 14;
-                        break;
-                    case "15":
-                        tmp = 45;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "12":
-            {
-                switch(b){
-                    case "13":
-                        tmp = 7;
-                        break;
-                    case "14":
-                        tmp = 10;
-                        break;
-                    case "15":
-                        tmp = 52;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "13":
-            {
-                switch(b){
-                    case "14":
-                        tmp = 9;
-                        break;
-                    case "15":
-                        tmp = 46;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "14":
-            {
-                switch(b){
-                    case "15":
-                        tmp = 41;
-                        break;
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        case "15":
-            {
-                switch(b){
-                    default :
-                        tmp = 0;
-                        break;
-                }
-            }
-            break;
-        default:
-            tmp = 0;
-            break;
-    }
-    if (tmp == 0){
+    var tmp = towns[a][b];
+
+    if (tmp < 0){
         $('#time').html('pas encore implementer');
         return;
     }
@@ -516,10 +57,10 @@ function setTime(a,b,c){
             $('#time').html(hourify(tmp/3));
             break;
         case "3":
-            $('#time').html(dayify(tmp/3));
+            $('#time').html(dayify(tmp/3,d));
             break;
         case "4":
-            $('#time').html(dayify(tmp));
+            $('#time').html(dayify(tmp,d));
             break;
     }
 }
@@ -533,8 +74,8 @@ function hourify(time){
     return ( h + ' heures ' + m + ' minutes');
 }
 
-function dayify(time){
-    var d = Math.floor(time/9);
+function dayify(time,hpd){
+    var d = Math.floor(time/hpd);
     if (d == time){
         return ( time + ' jours');
     }
